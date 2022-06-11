ede863d5a0a7723ffb0e030c4c6a13098e81f49e
Mr-Georgie <george.isiguzo@yahoo.com>
2022-06-11 22:37:08 +0100

    working on newsfeed page

--- src/components/Leftsidebar.js
+++ src/components/Leftsidebar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export default function Leftsidebar() {
   return (
-    <div className="flex flex-col items-center p-4">
+    <div className="flex flex-col p-4">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -22,7 +22,7 @@ export default function Leftsidebar() {
             d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
           />
         </svg>
-        <Link to="/" className="font-semibold tracking-tight text-3xl">
+        <Link to="/" className="font-semibold tracking-tight text-xl">
           Ideas
         </Link>
       </div>
