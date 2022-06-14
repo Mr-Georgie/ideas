48f9f6e81fe0506a0562357faac0ff2a949bf377
Mr-Georgie <george.isiguzo@yahoo.com>
2022-06-14 16:00:03 +0100

    edited index.js file to use react v17

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+// import ReactDOM from "react-dom/client"; - react v18.0.1
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
@@ -7,13 +8,13 @@ import { AnimationContextProvider } from "./context/AnimationContext";
 
 import { BrowserRouter as Router } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Router>
       <AnimationContextProvider>
         <App />
       </AnimationContextProvider>
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
