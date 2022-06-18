71d5eb1570f3be819a19d1b3c5f9727b840b4b5a
Mr-Georgie <george.isiguzo@yahoo.com>
2022-06-18 09:41:57 +0100

    improving mobile responsiveness

--- src/config.js
+++ src/config.js
@@ -1,38 +1,13 @@
 import { Appwrite } from "appwrite";
 
-/**
- * Digital ocean config
- */
-
-const sdk = new Appwrite()
+const sdk = new Appwrite();
 
 sdk
-   .setEndpoint("https://appwrite.georgeisiguzo.xyz/v1") // Your API Endpoint Ip "http://188.166.110.93/v1"
-   .setProject('6271a55fc848d4a07753') // Your project ID
-;
+  .setEndpoint("https://linode.georgeisiguzo.xyz/v1") // Your API Endpoint Ip "http://188.166.110.93/v1"
+  .setProject("62ad8f1b61a1aa4ac561"); // Your project ID
 
-const collectionId = "6271702a3a066954ffc0"
-const questionsCollectionId = "6271720fe9ea03bc27be"
-// const old_spacesCollectionId = "627172b4e8854a25d653"
-const bucketId = "6276bc698382d791a207"
+const collectionId = "6271702a3a066954ffc0";
+const questionsCollectionId = "6271720fe9ea03bc27be";
+const bucketId = "6276bc698382d791a207";
 
-export { sdk, collectionId, questionsCollectionId, bucketId }
-
-
-
-
-
-/**
- * Gitpod config
- * 
- * sdk
-    .setEndpoint("http://8080-appwrite-integrationfor-ghw26ygqb0r.ws-eu43.gitpod.io/v1") // Your API Endpoint
-    .setProject('626cf93661b59bdb8f3b') // Your project ID
-    ;
- * 
- *  const collectionId = "626cf9c5709e1b288e1e"
-    const questionsCollectionId = "626fdd0c71c45daab351"
-
-    export { sdk, collectionId, questionsCollectionId }
- * 
- */
+export { sdk, collectionId, questionsCollectionId, bucketId };
