e7811bf67ae26e4a781ae5fc4488b70e2b736255
Mr-Georgie <george.isiguzo@yahoo.com>
2022-06-20 21:44:43 +0100

    working on post page

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -33,6 +33,26 @@ export default function Post() {
             <p className="text-gray-600">Aug 18</p>
           </div>
         </div>
+
+        <div className="mb-10">
+          <p className=" text-custom-white text-base font-mono">
+            Hi there 👋 and welcome! I'm participating in the #HasuraHackathon
+            and in this article, I will be writing about the App I built and how
+            I built it. But firstly, allow me to talk about the inspiration
+            behind the App The Inspiration The App which I will call JobTracker
+            is majorly for aspiring programmers (like myself) who are on a goal
+            to secure their first "Tech" job. I noticed that in my attempts to
+            secure a Tech job so far, it has become overwhelming keeping track
+            of the requirements in each job ad I come across. There's always
+            that one important job requirement that I don't have which prevents
+            me from applying for the job. So I started thinking of different
+            ways I could keep track of job details and requirements so that I
+            can set goals around the most important one. That way, if I can
+            confidently tick the boxes for the important ones, I can overcome my
+            imposter syndrome and apply for that dream job I've long been
+            waiting for.
+          </p>
+        </div>
       </div>
     </div>
   );
